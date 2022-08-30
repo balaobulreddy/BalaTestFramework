1e46a1c278c150142d6983d7c460f3fa75ca92fe
Bala Obul Reddy <balaored@cisco.com>
2022-08-30 11:08:03 +0530

    Initial commit with framework.

---
+++ BalaTestFramework/SampleViewController.h
@@ -0,0 +1,18 @@
+//
+//  SampleViewController.h
+//  BalaTestFramework
+//
+//  Created by Bala Obul Reddy on 30/08/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SampleViewController : UIViewController
+
+@property (nonatomic) NSString *title;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BalaTestFramework/SampleViewController.m
@@ -0,0 +1,31 @@
+//
+//  SampleViewController.m
+//  BalaTestFramework
+//
+//  Created by Bala Obul Reddy on 30/08/22.
+//
+
+#import "SampleViewController.h"
+
+@interface SampleViewController ()
+
+@end
+
+@implementation SampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
